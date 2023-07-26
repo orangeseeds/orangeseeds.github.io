fe06f9614f2564f66c87214787d172334996c404
orangeseeds <shubhechchhakp@gmail.com>
2023-07-26 11:10:35 +0545

    Updates

--- src/lib/data.ts
+++
@@ -1,8 +0,0 @@
-
-export const posts = [
-    { title: "post #1", body: "body #1", slug: "1" },
-    { title: "post #2", body: "body #2", slug: "2" },
-    { title: "post #3", body: "body #3", slug: "3" },
-    { title: "post #4", body: "body #4", slug: "4" },
-]
-
