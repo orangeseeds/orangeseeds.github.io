1f4e96c0e95bd152d1d81abb065b6eed8b5d6474
orangeseeds <shubhechchhakp@gmail.com>
2023-09-01 12:39:26 +0545

    Updates

---
+++ _app/immutable/chunks/navigation.3b5dfc55.js
@@ -0,0 +1,1 @@
+import{h as a}from"./singletons.63f76fed.js";const e=a("after_navigate");export{e as a};
--- _app/immutable/chunks/navigation.a3d1e91c.js
+++
@@ -1,1 +0,0 @@
-import{h as a}from"./singletons.fab0de42.js";const e=a("after_navigate");export{e as a};
