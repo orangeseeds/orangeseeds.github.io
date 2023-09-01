efe3e784c1636d16047dec951f7cbd175943631c
orangeseeds <shubhechchhakp@gmail.com>
2023-09-01 12:11:25 +0545

    Updates

---
+++ _app/immutable/chunks/navigation.a3d1e91c.js
@@ -0,0 +1,1 @@
+import{h as a}from"./singletons.fab0de42.js";const e=a("after_navigate");export{e as a};
--- _app/immutable/nodes/5.8abea585.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.b108d059.js";import{S as t,i as e}from"../chunks/index.381cd29f.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
